7007be829c3c20f5c65e8e1c3ac5b41b62795959
Yahor <yahor.filipchyk@gmail.com>
2014-04-06 01:49:30 +0300

    Setting of host and port through command line arguments is added on server start

--- src/server.py
+++ src/server.py
@@ -1,15 +1,35 @@
 
+import sys
 import socket
-from client_communication import ServerThread
+from request_handler import request_handler
 
-HOST = "192.168.1.6"
+# default hostname and port
+HOST = "localhost"
 PORT = 8080
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(10)
+def start_server(host=None, port=None):
+    if host == None:
+        host = HOST
+    if port == None:
+        port = PORT
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((host, port))
+    except OSError:
+        print("Unable to start server on host [{0}] and bind it to port [{1}]. Try to use another hostname or port."
+            .format(host, port))
+        return
+    print("Server is started on host [{0}] and binded to port [{1}].".format(host, port))
+    server.listen(5)
+    while True:
+        conn, addr = server.accept()
+        client_connection = request_handler(addr, conn)
+        client_connection.process_request()
 
-while True:
-    conn, addr = server.accept()
-    thread = ServerThread(addr, conn)
-    thread.process_request()
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        start_server(host=sys.argv[1])
+    elif len(sys.argv) == 3:
+        start_server(host=sys.argv[1], port=int(sys.argv[2]))
+    else:
+        start_server()
